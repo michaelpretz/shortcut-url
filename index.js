aaf08e37ba92a67fda5f034788269c9dd5099bcd
Michael Pretz <40613721+michaelpretz@users.noreply.github.com>
2018-06-27 00:15:38 +0200

    simple fix for getting correct URLs from webloc files w/o %3C/string%3E at the end of the URL

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 'use strict';
+
 const path = require('path');
 const fs = require('fs');
 const getUrls = require('get-urls');
 const pify = require('pify');
 
 const getExt = () => {
-	switch (process.platform) {
-		case 'darwin':
-			return '.webloc';
-		case 'win32':
-			return '.url';
-		default:
-			return '.desktop';
-	}
+  switch (process.platform) {
+    case 'darwin':
+      return '.webloc';
+
+    case 'win32':
+      return '.url';
+
+    default:
+      return '.desktop';
+  }
 };
 
 module.exports = filepath => {
-	filepath += path.extname(filepath) ? '' : getExt();
+  filepath += path.extname(filepath) ? '' : getExt();
 
-	return pify(fs.readFile)(filepath, 'utf8')
-		.then(data => getUrls(data.replace(/<!doctype.*/i, ''))[0].trim())
-		.catch(err => {
-			if (err.code === 'ENOENT') {
-				err.message = `Couldn't find a web shortcut with the name \`${path.basename(`${filepath}\``)}`;
-			}
+  return pify(fs.readFile)(filepath, 'utf8')
+    .then(data => getUrls(data.replace(/<!doctype.*/i, '').replace(/<\/string>/i, ''))[0].trim())
+    .catch(err => {
+      if (err.code === 'ENOENT') {
+        err.message = `Couldn't find a web shortcut with the name \`${path.basename(`${filepath}\``)}`;
+      }
 
-			throw err;
-		});
+      throw err;
+    });
 };
